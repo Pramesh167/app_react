74b08bfe9943873ee85c54c7b94aa4f4506ecf44
pramesh167 <prameshpathak2002@gmail.com>
2024-04-28 13:28:01 +0545

    handlefunction

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -11,7 +11,28 @@ const Register = () => {
   const[confirmPassword,setConfirmPassword]=useState('')
 
   //make each function for changing value
-  
+  const handleFirstname=(e)=>{
+    setFirstName(e.target.value);
+
+  }
+  const handleLastname=(e)=>{
+    setLastName(e.target.value);
+
+  }
+  const handleEmail=(e)=>{
+    setEmail(e.target.value);
+
+  }
+  const handlePassword=(e)=>{
+    setPassword(e.target.value);
+
+  }
+  const handleConfirmpassword=(e)=>{
+    setConfirmPassword(e.target.value);
+
+  }
+
+
 
 
 
@@ -24,19 +45,20 @@ const Register = () => {
     <h1>Create an Account!</h1>
 
     <form className='w-50'>
-      <label>First Name</label>
-      <input type='text' className='form-control' placeholder='Enter your First Name'/>
+      <label>First Name :{firstName}</label>
+      <input onChange={handleFirstname} type='text' className='form-control' placeholder='Enter your First Name'/>
 
-      <label className='mt-4'>Last Name</label>
-      <input type='text' className='form-control' placeholder='Enter your Last Name'/>
+      <label className='mt-4'>Last Name:{lastName}</label>
+      <input onChange={handleLastname}type='text' className='form-control' placeholder='Enter your Last Name'/>
 
-      <label className='mt-4'>Email</label>
-      <input type='text' className='form-control' placeholder='Enter your Email Address'/>
+      <label className='mt-4'>Email:{email}</label>
+      <input onChange={handleEmail}type='text' className='form-control' placeholder='Enter your Email Address'/>
 
-      <label className='mt-4'>Password</label>
-      <input type='text' className='form-control' placeholder='Create your Password'/>
-      <label className='mt-4'>Confirm Password</label>
-      <input type='text' className='form-control' placeholder='Retype your Password to Confirm'/>
+      <label className='mt-4'>Password:{password}</label>
+      <input onChange={handlePassword} type='text' className='form-control' placeholder='Create your Password'/>
+
+      <label className='mt-4'>Confirm Password:{confirmPassword}</label>
+      <input onChange={handleConfirmpassword} type='text' className='form-control' placeholder='Retype your Password to Confirm'/>
 
 
       <button className='btn btn-light btn-outline-dark mt-4 w-100'>Create</button>
