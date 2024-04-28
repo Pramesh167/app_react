24516bf0f6835ff306029ae7f2d9dd3d041a2501
pramesh167 <prameshpathak2002@gmail.com>
2024-04-28 12:24:44 +0545

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+}from "react-router-dom";
+import Homepage from './pages/homepage/Homepage';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        {/* <Route path='/register'element={<h1>Register page</h1>}/>
+        <Route path='/login'element={<h1>Login page</h1>}/> */}
+      </Routes>
+    </Router>
   );
-}
+  
 
-export default App;
+}export default App;
---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar';
+
+const Homepage =()=>{
+    return(
+        <div>
+            
+            ImageSlider
+            Cards
+        </div>
+    )
+}
+export default Homepage;
